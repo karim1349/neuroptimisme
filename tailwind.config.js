631f3bbe1c315f28887493103f862f291479f9f8
Karim Benchekroun <karimbenchekroun@outlook.fr>
2023-12-28 20:34:28 +0100

    tailwindCSS added to project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'nude-50': '#FFFCF9',
+        'gold-50': '#AB8F4E',
+        'brown-25': '#E0BBA6',
+        'brown-50': '#BC6C3E',
+      },
+    },
+  },
+  plugins: [],
+}
+
