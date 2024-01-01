1ef02a269bd68d8d462b4286d63b5cc71400d9d7
Karim Benchekroun <karimbenchekroun@outlook.fr>
2024-01-01 19:42:24 +0100

    routes implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './index.css'
 import Home from './pages/Home/Home';
-import Footer from './pages/Home/components/Footer';
-import NavBar from './pages/Home/components/NavBar';
 function App() {
   return (
     <>
-      <NavBar />
       <Home />
-      <Footer />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import SuiviPsychologique from './pages/Home/SuiviPsychologique';
+import Psychoeducation from './pages/Home/Psychoeducation';
+import GuidanceParentale from './pages/Home/GuidanceParentale';
+import HabiliteSociale from './pages/Home/HabiliteSociale';
+import Bilan from './pages/Home/Bilan';
+import RemediationCognitive from './pages/Home/RemediationCognitive';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+      <HashRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/suivi_psychologique" element={<SuiviPsychologique />} />
+          <Route path="/psychoeducation" element={<Psychoeducation />} />
+          <Route path="/guidance_parentale" element={<GuidanceParentale />} />
+          <Route path="/habilite_sociale" element={<HabiliteSociale />} />
+          <Route path="/bilan" element={<Bilan />} />
+          <Route path="/remediation_cognitive" element={<RemediationCognitive />} />
+        </Routes>
+      </HashRouter>
+      <Footer />
+    </>
   </React.StrictMode>
 );
