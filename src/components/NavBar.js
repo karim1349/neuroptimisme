d13e255d2f64c53cda7bb9613121c88d1c0de932
Karim Benchekroun <karimbenchekroun@outlook.fr>
2024-01-01 21:01:18 +0100

    hover bug fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <div className="bg-brown-25 flex justify-between transition-all duration-500 ease-in-out">
+        <div className="bg-brown-25 flex justify-between transition-all duration-500 ease-in-out z-50">
             <img src="/assets/logo.png" alt="logo" className="w-20 h-20 object-contain" />
             <div className="flex items-center">
-                <Link to="/"         className="cursor-pointer mr-11 transition-colors duration-200 ease-in-out hover:text-brown-50">Accueil</Link>
+                <Link to="/"         className="cursor-pointer mr-11 transition-colors duration-200 ease-in-out hover:text-brown-50 z-50">Accueil</Link>
                 <div className="group flex justify-center cursor-pointer mr-11 transition-colors duration-200 ease-in-out hover:text-brown-50">
                     <span>Me connaître</span>
                     <svg className="w-5 h-5 inline-block ml-1 transform group-hover:rotate-180 ease-in-out duration-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M19 9l-7 7-7-7"></path></svg>
-                    <div className="absolute hidden group-hover:block">
+                    <div className="absolute hidden group-hover:block delay-100">
                         <div className="flex bg-white mt-9 rounded-md p-5 text-black">
                             <div>
                                 <Link to="/experiences" className="flex px-4 py-2 hover:bg-nude-50 hover:text-brown-50 items-center rounded-md">
@@ -22,7 +22,7 @@ const NavBar = () => {
                         </div>
                     </div>
                 </div>
-                <Link to="/a_propos"    className="cursor-pointer mr-11 transition-colors duration-200 ease-in-out hover:text-brown-50">À propos</Link>
+                <Link to="/a_propos"    className="cursor-pointer mr-11 transition-colors duration-200 ease-in-out hover:text-brown-50 z-50">À propos</Link>
                 <div className="group flex justify-center cursor-pointer mr-11 transition-colors duration-200 ease-in-out hover:text-brown-50">
                     <span>Mes services</span>
                     <svg className="w-5 h-5 inline-block ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M19 9l-7 7-7-7"></path></svg>
@@ -59,7 +59,7 @@ const NavBar = () => {
                         </div>
                     </div>
                 </div>
-                <Link to="/tarifs"  className="cursor-pointer mr-11 transition-colors duration-200 ease-in-out hover:text-brown-50">Mes tarifs</Link>
+                <Link to="/tarifs"  className="cursor-pointer mr-11 transition-colors duration-200 ease-in-out hover:text-brown-50 z-50">Mes tarifs</Link>
             </div>
         </div>
     )
